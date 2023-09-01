118be2aed4d8fba55a2428e52b059dc03576b222
Nirob Hasan <nirob427417@gmail.com>
2023-09-01 10:57:43 +0600

    auth login

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,52 @@
+import { getUserByEmail, isPasswordMatched } from './auth.utils';
+
+import httpStatus from 'http-status';
+
+import { Secret } from 'jsonwebtoken';
+import config from '../../../config';
+import ApiError from '../../../errors/ApiError';
+import { jwtHelpers } from '../../../helpers/jwtHelpers';
+import { ILoginUser, ILoginUserResponse } from './auth.interface';
+
+const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
+  const { email, password } = payload;
+
+  if (!email || !password) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'email & password needed');
+  }
+  const isUserExist = await getUserByEmail(email);
+
+  if (!isUserExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist');
+  }
+  if (
+    isUserExist.password &&
+    !(await isPasswordMatched(password, isUserExist.password))
+  ) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect');
+  }
+
+  //create access token & refresh token
+  const { id, role } = isUserExist;
+
+  const accessToken = jwtHelpers.createToken(
+    { id, role },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+  const refreshToken = jwtHelpers.createToken(
+    { id, role },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
+
+export const AuthService = {
+  loginUser,
+};
